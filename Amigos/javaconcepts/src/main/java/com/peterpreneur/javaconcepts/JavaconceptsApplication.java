da4b38b00be6da60b199343efa42883b783a79fc
Peter S <peter@Mac.lan>
2026-09-24 13:23:04 -0500

    add dependencies

---
+++ Amigos/javaconcepts/src/main/java/com/peterpreneur/javaconcepts/JavaconceptsApplication.java
@@ -0,0 +1,13 @@
+package com.peterpreneur.javaconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaconceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaconceptsApplication.class, args);
+	}
+
+}
---
+++ Amigos/javaconcepts/src/test/java/com/peterpreneur/javaconcepts/JavaconceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peterpreneur.javaconcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaconceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
