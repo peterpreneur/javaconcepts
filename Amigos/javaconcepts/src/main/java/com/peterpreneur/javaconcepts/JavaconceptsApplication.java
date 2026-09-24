00fe9ca287720a729e10ed3b042a0ebde6846d47
Peter S <peter@Mac.lan>
2026-09-24 13:28:45 -0500

    add beancount

--- Amigos/javaconcepts/src/main/java/com/peterpreneur/javaconcepts/JavaconceptsApplication.java
+++ Amigos/javaconcepts/src/main/java/com/peterpreneur/javaconcepts/JavaconceptsApplication.java
@@ -2,12 +2,14 @@ package com.peterpreneur.javaconcepts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class JavaconceptsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaconceptsApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(JavaconceptsApplication.class, args);
+		System.out.println("Bean Count:" + context.getBeanDefinitionCount());
 	}
 
 }
